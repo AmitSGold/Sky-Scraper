c90d3eac21fa02db0b8711b7fa749a9db6f027ae
AmitSGold <43923570+AmitSGold@users.noreply.github.com>
2020-11-23 11:07:48 +0200

    Update Sky Scraper.py

--- Sky Scraper.py
+++ Sky Scraper.py
@@ -2,6 +2,19 @@ import bs4
 from selenium import webdriver
 import time
 
+DESTINATION_TARGET = "fsc-destination-search"
+ONE_WAY_BUTTON = "BpkRadio_bpk-radio__3J6D-.TripTypeSelector_TripTypeSelector__1qmS8"
+DEPART_DATE_BUTTON = "depart-fsc-datepicker-button"
+RETURN_DATE_PICKER = "return-fsc-datepicker-button"
+DATE_PICKER = "BpkCalendarGrid_bpk-calendar-grid__date__3Dy_6"
+NO_PASSANGERS_BOX = "CabinClassTravellersSelector_CabinClassTravellersSelector__triggertext__3e3km"
+PASSANGERS_ADD_BUTTON = "BpkButtonBase_bpk-button__3QGRV.BpkButtonBase_bpk-button--icon-only__3Ww9s.BpkButtonSecondary_bpk-button--secondary__1quDO.BpkNudger_bpk-nudger__button__1RgFI"
+PASSANGERS_DONE_XPATH = '//*[@id="cabin-class-travellers-popover"]/footer/button'
+START_SEARCH_XPATH = '//*[@id="flights-search-controls-root"]/div/div/form/div[3]/button'
+
+
+NON_STOP_CONT = "BpkText_bpk-text__2VouB.BpkText_bpk-text--lg__1PdnC.BpkText_bpk-text--bold__NhE9P"
+
 def input_number(prompt):
     while True:
         try:
@@ -22,25 +35,28 @@ class Flight_Parameters():
  
         if one_way.lower() == 'y':
             self.oneway = True
+            self.return_date = None
+        
         else:
             self.oneway = False
-            self.depart_date = input_number("Depart ")
-            self.return_date = None
-    
+            
+        
+        self.depart_date = input_number("Depart ")
+        
         if not self.oneway:
             self.return_date = input_number("Return ")
     
-        self.pax = input_number("Nos. of passengers ")
+        self.passengers = input_number("Nos. of passengers ")
     
 
 class Sky_Scraper:
     
-    def __init__(self, where = '', one_way = False, dep = 0, ret = 0, pax = 1):
+    def __init__(self, where = '', one_way = False, dep = 0, ret = 0, passengers = 1):
         self.where = where
         self.one_way = one_way
         self.dep = dep
         self.ret = ret
-        self.pax = pax
+        self.passengers = passengers
     
     def set_driver(self):
         options = webdriver.ChromeOptions()
@@ -53,43 +69,43 @@ class Sky_Scraper:
         self.soup = soup
         
     def set_destination(self,sleep_time = 1):
-        to = self.driver.find_element_by_id("fsc-destination-search")
+        to = self.driver.find_element_by_id(DESTINATION_TARGET)
         to.send_keys(" " + self.where.title())
         time.sleep(sleep_time)
         
     def set_one_way(self, sleep_time = 2):
         if self.one_way:
-         oneway = self.driver.find_elements_by_class_name("BpkRadio_bpk-radio__input__1OAwe")[1]
+         oneway = self.driver.find_elements_by_class_name(ONE_WAY_BUTTON)[1]
          oneway.click()
          time.sleep(sleep_time)   
             
-    def set_depart_day(self,sleep_time = 1):
-        departure = self.driver.find_element_by_id("depart-fsc-datepicker-button")
+    def set_depart_day(self,sleep_time = 3):
+        departure = self.driver.find_element_by_id(DEPART_DATE_BUTTON)
         departure.click()
         time.sleep(sleep_time)
-        dep_dates = self.driver.find_elements_by_class_name("BpkCalendarDate_bpk-calendar-date__MuPBz")
-        dep_date = dep_dates[self.dep+1]
+        dep_dates = self.driver.find_elements_by_class_name(DATE_PICKER)
+        dep_date = dep_dates[self.dep-1]
         dep_date.click()
         time.sleep(sleep_time)
     
     def set_return_day(self,sleep_time = 1):
-        ret = self.driver.find_element_by_id("return-fsc-datepicker-button")
+        ret = self.driver.find_element_by_id(RETURN_DATE_PICKER)
         ret.click()
         time.sleep(sleep_time)
-        ret_dates = self.driver.find_elements_by_class_name("BpkCalendarDate_bpk-calendar-date__MuPBz")
-        ret_date = ret_dates[self.ret+1]
+        ret_dates = self.driver.find_elements_by_class_name(DATE_PICKER)
+        ret_date = ret_dates[self.ret-1]
         ret_date.click()
         time.sleep(sleep_time)
         
     def set_no_of_passengers(self, sleep_time = 1):
-        pas = self.driver.find_element_by_class_name("CabinClassTravellersSelector_CabinClassTravellersSelector__triggertext__2uAq3")    
+        pas = self.driver.find_element_by_class_name(NO_PASSANGERS_BOX)    
         pas.click()
         time.sleep(sleep_time)
-        more = self.driver.find_elements_by_class_name("BpkButtonBase_bpk-button__1pnhi.BpkButtonBase_bpk-button--icon-only__3J3rW.BpkButtonSecondary_bpk-button--secondary__1-kNc.BpkNudger_bpk-nudger__button__YOZET")[1]
-        for passenger in range(self.pax-1):
+        more = self.driver.find_elements_by_class_name(PASSANGERS_ADD_BUTTON)[1]
+        for passenger in range(self.passengers-1):
             more.click()
             time.sleep(sleep_time)
-        done = self.driver.find_element_by_xpath('//*[@id="cabin-class-travellers-popover"]/footer/button')
+        done = self.driver.find_element_by_xpath(PASSANGERS_DONE_XPATH)
         done.click()
         time.sleep(sleep_time+1)
             
@@ -104,10 +120,10 @@ class Sky_Scraper:
         else:
             self.set_return_day()
         
-        if self.pax > 1:
+        if self.passengers > 1:
             self.set_no_of_passengers()
         
-        go = self.driver.find_element_by_xpath('//*[@id="flights-search-controls-root"]/div/div/form/div[3]/button')
+        go = self.driver.find_element_by_xpath(START_SEARCH_XPATH)
         go.click()
         
     def close_login_popup(self):
@@ -125,7 +141,7 @@ class Sky_Scraper:
           print("No flights found for this destination on these dates")
           return True
       elif "Want non-stop flights" or "Want a non-stop flight" in self.soup.text:
-          go = self.driver.find_element_by_class_name("BpkText_bpk-text__2NHsO.BpkText_bpk-text--lg__3vAKN.BpkText_bpk-text--bold__4yauk")
+          go = self.driver.find_element_by_class(NON_STOP_CONT)
           go.click()
           self.get_new_URL()        
           self.close_login_popup()
@@ -188,5 +204,5 @@ class Sky_Scraper:
             return tickets_list
 
 my_parameters = Flight_Parameters()               
-my_search = Sky_Scraper(my_parameters.where,my_parameters.oneway,my_parameters.depart_date,my_parameters.return_date,my_parameters.pax)
+my_search = Sky_Scraper(my_parameters.where,my_parameters.oneway,my_parameters.depart_date,my_parameters.return_date,my_parameters.passengers)
 tickets_list = my_search.get_tickets_list()           
